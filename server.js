5a4fa929993f8ce6e7748f834fd93277c701ebe6
Prajwal <pirateoncrew@gmail.com>
2021-06-08 14:45:16 +0545

    server setup

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const path = require("path");
-const favicon = require("express-favicon");
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 const app = express();
-app.use(favicon(__dirname + "/build/favicon.jpg"));
 
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
